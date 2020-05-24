23a9ecc99a84f7c4342b5933249dd70fede1a430
hellosamuel <samuelis419@gmail.com>
2020-05-25 00:06:05 +0900

    ADD body parser, winston for logging system

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import Koa from 'koa'
+import koaBody from 'koa-body'
 import Config from './config'
+import { requestLogger, errorLogger } from './logger'
 import Router from './router'
 
 const app = new Koa()
+
+app.use(koaBody())
+app.use(requestLogger).use(errorLogger)
 
 app.use(Config.server.cors())
 app.use(Router.routes()).use(Router.allowedMethods())
